1051b03697324db1357cb443af71cb661238ad51
atul080 <atulsrivastav16k26@gmaial.com>
2022-01-11 09:20:48 +0530

    Added: Card initialized with new changes after review

--- DeckOfAce/src/com/deckoface/DeckOfAceMain.java
+++ DeckOfAce/src/com/deckoface/DeckOfAceMain.java
@@ -3,35 +3,42 @@
  * @date 11/01/22
  * */
 package com.deckoface;
-
 import java.util.Random;
-
 public class DeckOfAceMain {
 	//initializing string arrays for suit and rank
 	public static final String rank[]={"2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"};
 	public static final String suit[]={"Clubs","Diamond","Hearts","Spades"};
+	public static String cards[]= new String[52];
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Welcome to Deck Of Ace program.");
-		
-		//initializing the cards
-		int n=suit.length*rank.length;
+		cardsInitialization();
+		print();
+
+	}
+	/* method to initialize the cards
+	 * loops to get all the possible card combinations
+	 */
+	public static void cardsInitialization()
+	{
+		//declaration to index over cards array.
 		int k=0;
-		String deck[]= new String[n];
 		for(int i=0;i<rank.length;i++)
 		{
 			for(int j=0;j<suit.length;j++)
 			{
-				deck[k]=rank[i]+" of "+suit[j];
+				cards[k]=rank[i]+" of "+suit[j];
 				k++;
 			}
 		}
-		
-		//prinitng the card
-		for(int i=0;i<n;i++)
-			System.out.println(deck[i]);
-
+	}
+	/* method to print the cards
+	 * loops to get all the combinations and printing it.
+	 */
+	public static void print()
+	{
+		for(int i=0;i<52;i++)
+			System.out.println(cards[i]);
 	}
 
 }
